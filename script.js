0f1e97050869ac84c14c0b6c52b2e6221ca125ab
minhnhatt10-coder <minhnhatt10@gmail.com>
2025-10-16 16:59:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ class FreeTranslationService {
     // PHƯƠNG THỨC PHÁT HIỆN NGÔN NGỮ
     async detectLanguage(text) {
         try {
-            const response = await fetch('https://api.detectlanguage.com/v2.0/detect', {
+            const response = await fetch('https://api.detectlanguage.com/private', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -348,5 +348,6 @@ window.addEventListener('offline', function() {
         window.translationApp.updateStatus('❌ Mất kết nối internet');
     }
 });
+
 
 
